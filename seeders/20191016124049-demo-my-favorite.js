b017a5edcdb3dad700fded8e74421d5f1d9661f3
andikas95-dev <andikaperdana95@gmail.com>
2019-11-15 08:54:03 +0700

    edit my favorite

--- seeders/20191016124049-demo-my-favorite.js
+++ seeders/20191016124049-demo-my-favorite.js
@@ -3,31 +3,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('my_favorites', [
-      {
-        webtoon_id: 1,
-        user_id: 1,
-        isFavorite: 1
-      },
-      {
-        webtoon_id: 1,
-        user_id: 2,
-        isFavorite: 1
-      },
-      {
-        webtoon_id: 1,
-        user_id: 2,
-        isFavorite: 1
-      },
-      {
-        webtoon_id: 2,
-        user_id: 1,
-        isFavorite: 1
-      },
-      {
-        webtoon_id: 2,
-        user_id: 2,
-        isFavorite: 1
-      },
     ], {});
   },
 
